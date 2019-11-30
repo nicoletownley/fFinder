6d719f7d6a6c4173cb54122dfd44ed728054c136
nicoletownley <natownley@gmail.com>
2019-11-30 16:55:12 -0600

    update picture and friends score

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 let friends = [
 
   {
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Wonderwoman",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzFGxhOeBqkrDv6zRlYu_DLrCjcQf4Zhw3Lg8Ef4v_t0Y8ijvTldZ0fbdI&s",
     "scores": [
       5,
       1,
@@ -17,18 +17,99 @@ let friends = [
     ]
   },
   {
-    "name": "Sheila",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/343522.jpg",
+    "name": "Superman",
+      "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvgeLAqdz-5Ac9n4v75yupSDWEBIxKPnxAsXzYh2_w8fph9quLLgLTdnE&s",
+      "scores": [
+        3,
+        1,
+        2,
+        4,
+        5,
+        5,
+        2,
+        1,
+        1,
+        1
+      ]
+    },
+    {
+      "name": "Joker",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcXb8mRnB4jEqPu-1zF6ApVZK3YZxkeLXG_wuSId4RqcceNJ6Z_zL9Y8wY&s",
+        "scores": [
+          3,
+          1,
+          2,
+          4,
+          1,
+          1,
+          2,
+          1,
+          1,
+          1
+        ]
+      },
+  
+  {
+    "name": "Incredible Hulk",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRnFLiVC65Us5UwnCHNAzUy7XspQqRYEdbYwOvKwZayCwQ6K5ogKSzmKE&s",
+    "scores": [
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+  },
+  {
+    "name": "Spiderman",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdLGezISL9AVyCXVTbnVhwIrPEj3eNUDtG8UMP1pvWtCufInyrSVIr1ASD&s",
     "scores": [
       5,
       5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5
+    ]
+  },
+  {
+    "name": "Catwoman",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6_4n7CDEhZSYn7oA6kqsDuHLDtUFIsRkpIX9PB1KDR7cuwCEcl1E14erH7w&s",
+    "scores": [
+      3,
+      1,
+      1,
+      5,
+      2,
+      1,
+      2,
+      5,
       4,
-      4,
+      1
+    ]
+  },
+  {
+    "name": "Batman",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8BZRrAxwtHZdxnJ6oeJ8iwtR78MY3O3p8MxjvR8uLyohJcJuvPLTCOyA&s",
+    "scores": [
+      1,
+      1,
+      2,
+      ,
       5,
       1,
       3,
       1,
-      4,
+      3,
       1
     ]
   },
